3cb57021fd3690f996d47e06ca084d7ac2d53a91
GuiJesus07 <guilhermejesus234@gmail.com>
2024-05-28 19:59:51 +0100

    post

--- BackEnd/controllers/CommentsController.js
+++ BackEnd/controllers/CommentsController.js
@@ -6,11 +6,40 @@ exports.getAllComments = (req, res) => {
 };
 
 exports.deleteComment = (req, res) =>{
-
+    const commentID = req.params.id;
+    Comment.destroy({
+        where: { commentID: commentID }
+    })
+    .then(deleted => {
+        if (deleted) {
+            res.send({ message: 'Comment deleted successfully' });
+        } else {
+            res.status(404).send({ message: 'Comment not found' });
+        }
+    })
+    .catch(error => {
+        res.status(500).send({ error: 'An error occurred while deleting the comment' });
+    });
 };
 
 exports.createComment = (req, res) =>{
-
+    const { commentText, commentDate, userID, tweetID } = req.body;
+    if (!commentText|| !commentDate || !userID || !tweetID) {
+        res.send({ message: 'comment uploaded successfully' });
+        return
+    }
+    Comment.create({
+        commentText: commentText,
+        commentDate: commentDate,
+        userID: userID,
+        tweetID: tweetID
+    })
+    .then(comment => {
+        res.status(201).send(comment);
+    })
+    .catch(error => {
+        res.status(500).send({ error: 'An error occurred while creating the comment' });
+    });
 };
 
 exports.updateComment = (req, res) =>{
--- BackEnd/controllers/PostsController.js
+++ BackEnd/controllers/PostsController.js
@@ -13,17 +13,32 @@ exports.deletePost = (req, res) => {
     })
     .then(deleted => {
         if (deleted) {
-            res.send({ message: 'Post deleted successfully' });
+            res.send({ message: 'Tweet deleted successfully' });
         } else {
-            res.status(404).send({ message: 'Post not found' });
+            res.status(404).send({ message: 'Tweet not found' });
         }
     })
     .catch(error => {
-        res.status(500).send({ error: 'An error occurred while deleting the post' });
+        res.status(500).send({ error: 'An error occurred while deleting the tweet' });
     });
 };
 exports.createPost = (req, res) =>{
-
+    const { tweetText, tweetDate, userID} = req.body;
+    if (!tweetText || !tweetDate || !userID) {
+        res.send({ message: 'Tweet created successfully' });
+        return
+    }
+    Post.create({
+        tweetText: tweetText,
+        tweetDate: tweetDate,
+        userID: userID
+    })
+    .then(user => {
+        res.status(201).send(user);
+    })
+    .catch(error => {
+        res.status(500).send({ error: 'An error occurred while creating the tweet' });
+    });
 };
 
 exports.updatePost = (req, res) =>{
